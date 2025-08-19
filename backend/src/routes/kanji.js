1d06fc068a20f6a83a120831173abac4b537117f
Solka99 <alicjas508@gmail.com>
2025-08-19 10:27:23 +0200

    Delete unnecessary files

--- backend/src/routes/kanji.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const kanjiController = require("../controllers/kanjiController");
-
-router.get("/", kanjiController.getAllKanji);
-router.get("/:id", kanjiController.getKanjiById);
-router.post("/search", kanjiController.searchKanji);
-
-module.exports = router;
--- mobile/App.tsx
+++
@@ -1,7 +0,0 @@
-// App.tsx
-import React from "react";
-import HomeScreen from "./src/screens/home/HomeScreen"; // パスを適切に調整
-
-export default function App() {
-  return <HomeScreen />;
-}
